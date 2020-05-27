939f51e673f658a355d71a4702b8ed8da0d91eda
Otabek Jurayev <otabek.jurayev.2002@gmail.com>
2020-05-27 23:29:08 +0500

    Две страницы готовы

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.carousel__box').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.reviews__box').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+});
+
+$('.news__box').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+});
